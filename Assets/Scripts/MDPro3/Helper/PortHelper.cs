5825fcb25d6c0e03c3cd99e5b74dd87f3326262e
SherryChaos <60814455+SherryChaos@users.noreply.github.com>
2024-04-17 12:03:01 +0800

    new hint for importing

--- Assets/Scripts/MDPro3/Helper/PortHelper.cs
+++ Assets/Scripts/MDPro3/Helper/PortHelper.cs
@@ -88,19 +88,35 @@ namespace MDPro3
             bool newDataAdded = false;
             foreach (string path in files)
             {
+                var fileName = Path.GetFileName(path);
                 try
                 {
                     if (path.ToLower().EndsWith(".ydk"))
-                        File.Copy(path, Program.deckPath + Program.slash + Path.GetFileName(path), true);
-                    if (path.ToLower().EndsWith(".yrp") || path.ToLower().EndsWith(".yrp3d"))
-                        File.Copy(path, Program.replayPath + Program.slash + Path.GetFileName(path), true);
-                    if (path.ToLower().EndsWith(".ypk") || path.ToLower().EndsWith(".zip") || path.ToLower().EndsWith(".cdb") || path.ToLower().EndsWith(".conf"))
                     {
-                        File.Copy(path, Program.expansionsPath + Program.slash + Path.GetFileName(path), true);
-                        newDataAdded = true;
+                        File.Copy(path, Program.deckPath + Program.slash + fileName, true);
+                        MessageManager.Cast(InterString.Get("导入卡组「[?]」成功。", fileName.Replace(".ydk", string.Empty)));
                     }
-                    if (path.ToLower().EndsWith(".png") || path.ToLower().EndsWith(".jpg") || path.ToLower().EndsWith(".jpeg"))
+                    else if (path.ToLower().EndsWith(".yrp") || path.ToLower().EndsWith(".yrp3d"))
+                    {
+                        File.Copy(path, Program.replayPath + Program.slash + fileName, true);
+                        MessageManager.Cast(InterString.Get("导入回放「[?]」成功。", fileName));
+                    }
+                    else if (path.ToLower().EndsWith(".ypk") || path.ToLower().EndsWith(".zip") || path.ToLower().EndsWith(".cdb") || path.ToLower().EndsWith(".conf"))
+                    {
+                        File.Copy(path, Program.expansionsPath + Program.slash + fileName, true);
+                        newDataAdded = true;
+                        if (fileName.ToLower().EndsWith(".ypk") || fileName.ToLower().EndsWith(".zip"))
+                            MessageManager.Cast(InterString.Get("导入扩展卡文件「[?]」成功。", fileName));
+                        else if (fileName.ToLower().EndsWith(".cdb"))
+                            MessageManager.Cast(InterString.Get("导入卡片数据库「[?]」成功。", fileName));
+                        else if (fileName.ToLower().EndsWith(".conf"))
+                            MessageManager.Cast(InterString.Get("导入字段文件「[?]」成功。", fileName));
+                    }
+                    else if (path.ToLower().EndsWith(".png") || path.ToLower().EndsWith(".jpg"))
+                    {
                         File.Copy(path, Program.altArtPath + Program.slash + Path.GetFileName(path), true);
+                        MessageManager.Cast(InterString.Get("导入自定义卡图「[?]」成功。", fileName));
+                    }
                 }
                 catch { }
             }
